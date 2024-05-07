ce2501548e2392107a9d6170fcc48c2137e01927
gugrigugra <gabrieledicosmo05@gmail.com>
2024-05-07 17:49:55 +0200

    cambiamenti

--- backend/api.php
+++ backend/api.php
@@ -1,6 +1,13 @@
 <?php
 require_once("connect.php");
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Inizializza l'ambiente Twig
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if ($_SERVER['PATH_INFO'] == '/movies') {
 
@@ -23,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
         $movies = get_movies($user_input, $filter);
 
+        echo $twig->render('movies.twig', ['movies' => $movies]);
 
         http_response_code(200);
         header("Content-Type: application/json");
