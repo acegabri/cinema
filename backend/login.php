77f4ecebf0bcdaf8a0ce87bdcec79824901ae015
gugrigugra <gabrieledicosmo05@gmail.com>
2024-05-15 17:17:17 +0200

    risoluzione problemi di login. inizio implementazione stile del progetto

--- backend/login.php
+++ backend/login.php
@@ -4,41 +4,44 @@ session_start();
 
 require_once('connect.php');
 require_once '../vendor/autoload.php';
-$loader = new \Twig\Loader\FilesystemLoader('../templates'); 
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 
+$error = '';
+
+/* 
 if (isset($_SESSION['email'])) {
-    header('Location: api.php/');
-} else {
+    header('Location: ../api.php/');
+    exit();
+}
+*/
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        $user = array();
+    // Connessione al database
+    $conn = mysqli_connect("mysql", "root", "root", "db_film");
 
-        $user['email'] = $email;
-        $user['password'] = $password;
-
-        $conn = mysqli_connect("mysql", "root", "root", "db_film");
-
-        if (!$conn) {
-            die('Connection failed: ' . mysqli_connect_error());
-        }
-
-        $query = "SELECT * FROM users WHERE email like '$email' AND password like  '$password'";
-        $result = mysqli_query($conn, $query);
-
-        if (mysqli_num_rows($result) > 0) {
-            
-            $row = mysqli_fetch_assoc($result);
-            //echo $row;
-            assign_sess_values($row);
-            header('Location: api.php/');
-        } else {
-            echo "email or password incorrect!";
-        }
+    if (!$conn) {
+        die('Connection failed: ' . mysqli_connect_error());
     }
+
+    // Query senza prepared statements (non sicuro)
+    $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        assign_sess_values($row);
+        header('Location: ../backend/api.php/');
+        exit();
+    } else {
+        header('Location: ../backend/api.php/registrazione.html.twig');
+    }
+
+    mysqli_close($conn);
+    exit();
 }
 
 function assign_sess_values($row)
